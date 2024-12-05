f4cc1c8d0fb6b440c791dec40cb7ddb92dcedbd3
Konrad Wasowicz <exaroth@gmail.com>
2024-12-05 17:40:32 +0100

    add target path arg

--- src/errors.rs
+++ src/errors.rs
@@ -5,8 +5,8 @@ use std::path::PathBuf;
 /// to filesystem such as missing files.
 #[derive(Error, Debug)]
 pub enum FilesystemError {
-    #[error("Invalid path provided: `{0}`")]
-    InvalidPathProvided(String),
+    // #[error("Invalid path provided: `{0}`")]
+    // InvalidPathProvided(String),
     #[error("File or directory does not exist: `{0}`")]
     PathDoesNotExist(PathBuf),
     #[error("Looks like liveboat has not been initialized, run liveboat -x init first.")]
