7d7f440443c46271fae42af0daefcfafd59bc913
Konrad Wasowicz <exaroth@gmail.com>
2024-12-07 19:41:30 +0100

    add backup text for modal

--- templates/default/src/include/src/components/EmbedModal.vue
+++ templates/default/src/include/src/components/EmbedModal.vue
@@ -24,9 +24,12 @@ const props = defineProps({
 
 <template>
   <div id="embed-modal-overlay" @click="embedStore.hideEmbedModal()">
-    <div v-if="props.embedCode" id="modal-container" v-html="props.embedCode"></div>
+    <div v-if="props.embedCode">
+      <p class="modal-text-bkp">Loading...</p>
+      <div id="modal-container" v-html="props.embedCode"></div>
+    </div>
     <div v-else>
-      <a :href="props.fallbackUrl"></a>
+      <a :href="props.fallbackUrl" class="modal-text-bkp">{{ props.fallbackUrl }}</a>
     </div>
     <a id="modal-close" @click="embedStore.hideEmbedModal()"><IconClose /></a>
   </div>
@@ -55,6 +58,14 @@ const props = defineProps({
   width: 70vw;
   height: 90vh;
 }
+.modal-text-bkp {
+  display: block;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  position: fixed;
+  font-size: 1.3rem;
+}
 #modal-close {
   display: block;
   position: absolute;
@@ -64,7 +75,7 @@ const props = defineProps({
   right: 50px;
   cursor: pointer;
   z-index: 999;
-  opacity: .6;
+  opacity: 0.6;
 }
 
 @media (max-width: 1280px) {
