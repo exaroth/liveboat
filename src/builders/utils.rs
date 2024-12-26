6fb29855bea2bc6f47fee377e89bbcf9d5660cb3
Konrad Wasowicz <exaroth@gmail.com>
2024-12-26 23:14:41 +0100

    Presort feed items before adding to rss feed

--- src/builders/utils.rs
+++ src/builders/utils.rs
@@ -1,4 +1,4 @@
-
+use log::info;
 use rss::ChannelBuilder;
 
 use crate::feed::Feed;
@@ -6,6 +6,7 @@ use crate::opts::Options;
 
 /// Generate RSS channel for the liveboat page.
 pub fn generate_rss_channel(opts: &Options, feeds: &Vec<Feed>) -> String {
+    info!("Generating rss channel");
     let mut channel = ChannelBuilder::default()
         .title(&opts.title)
         // TODO: add url to options
@@ -15,9 +16,12 @@ pub fn generate_rss_channel(opts: &Options, feeds: &Vec<Feed>) -> String {
     let mut items = Vec::new();
     for feed in feeds {
         for feed_item in feed.clone().items {
-            items.push(feed_item.to_rss_item())
+            items.push(feed_item)
         }
     }
-    channel.set_items(items);
+    items.sort_by(|a, b| a.date().cmp(&b.date()));
+    for item in items {
+        channel.items.push(item.to_rss_item())
+    }
     return channel.to_string()
 }
