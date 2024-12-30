95618daca5eb45f9fe2da3dc5f3faa448a0714c6
Konrad Wasowicz <exaroth@gmail.com>
2024-12-30 14:14:39 +0100

    Add OPML icon

---
+++ templates/default/src/include/src/components/icons/IconOPML.vue
@@ -0,0 +1,31 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    version="1.1"
+    width="1080"
+    height="1080"
+    viewBox="0 0 1080 1080"
+    xml:space="preserve"
+  >
+    <g transform="matrix(0.87 0 0 0.87 540 540)" id="SVGRepo_iconCarrier">
+      <path
+        style="
+          stroke: none;
+          stroke-width: 1;
+          stroke-dasharray: none;
+          stroke-linecap: butt;
+          stroke-dashoffset: 0;
+          stroke-linejoin: miter;
+          stroke-miterlimit: 4;
+          fill: currentColor;
+          fill-rule: nonzero;
+          opacity: 1;
+        "
+        transform=" translate(-500, -626)"
+        d="M 271 226 Q 224 226 185 249 Q 146 272 123 311.5 Q 100 351 100 397 L 100 856 Q 100 902 123 941.5 Q 146 981 185 1003.5 Q 224 1026 271 1026 L 729 1026 Q 776 1026 815 1003.5 Q 854 981 877 941.5 Q 900 902 900 856 L 900 397 Q 900 351 877 311.5 Q 854 272 815 249 Q 776 226 729 226 L 271 226 z M 502 336 Q 580 336 648 376 Q 714 415 752 480 Q 792 548 792 627 Q 792 706 752 773 Q 714 839 648 878 Q 580 918 501.5 918 Q 423 918 355 878 Q 289 839 251 773 Q 211 706 211 627 Q 211 548 251 480 Q 289 415 355 376 Q 423 336 502 336 z M 502 424 Q 446 424 399.5 451.5 Q 353 479 325.5 525.5 Q 298 572 298 627 Q 298 682 325.5 728.5 Q 353 775 399.5 802.5 Q 446 830 501.5 830 Q 557 830 603.5 802.5 Q 650 775 677.5 728.5 Q 705 682 705 627 Q 705 572 677.5 525.5 Q 650 479 603.5 451.5 Q 557 424 502 424 z M 502 562 Q 531 562 552 583 Q 573 604 573 633.5 Q 573 663 552 684 Q 531 705 501.5 705 Q 472 705 451 684 Q 430 663 430 633.5 Q 430 604 451 583 Q 472 562 502 562 z"
+        stroke-linecap="round"
+      />
+    </g>
+  </svg>
+</template>
